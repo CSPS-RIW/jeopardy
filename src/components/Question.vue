338fb8fd20c5d61045a9bfaa437ad2b1e7c9c257
noamstul <stul0002@algonquinlive.com>
2024-04-17 11:06:28 -0400

    adding better html to question

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -3,23 +3,30 @@
     <div>
       <h2>Question</h2>
       <p v-if="question">{{ question.question }}</p>
-      <div v-if="question">
-        <div v-for="(option, index) in question.options" :key="index" class="option" @click="checkAnswer(option)">
-          {{ option }}
-        </div>
+      <form @submit.prevent="checkAnswer" v-if="question">
+      <div v-for="(option, index) in question.options" :key="index" class="option">
+        <input type="radio" :id="'option_' + index" :value="option" v-model="selectedOption">
+        <label :for="'option_' + index">{{ option }}</label>
       </div>
+      <button type="submit" :disabled="!selectedOption">Submit</button>
+    </form>
       <p v-else>No question found.</p>
+      <button @click="goBack" :disabled="!selectedOption">Back to Game Board</button>
     </div>
   </template>
   
   <script setup>
   import { ref, onMounted } from 'vue'
-  import { useRoute } from 'vue-router'
+  import { useRoute, useRouter } from 'vue-router'
   import gameData from '../gameData.json'
   
   const route = useRoute()
+  const router = useRouter()
   const questionId = ref(route.params.id)
   const question = ref(null)
+
+  const selectedOption = ref('')
+const score = ref(0)
   
   onMounted(() => {
     question.value = gameData.questions.find(q => q.id === parseInt(questionId.value))
@@ -28,22 +35,21 @@
     }
   })
   
-  const checkAnswer = (option) => {
-    if (!question.value) {
-      console.error('Question is not defined.')
-      return
-    }
-  
-    if (option === question.value.answer) {
-      // Correct answer
-      // Add logic to update score or perform other actions
-      alert('Correct!')
-    } else {
-      // Incorrect answer
-      // Add logic to handle incorrect answers
-      alert('Incorrect. Try again!')
-    }
+  const checkAnswer = () => {
+  if (selectedOption.value === question.value.answer) {
+    // Correct answer
+    score.value += question.value.value
+    alert('Correct!')
+  } else {
+    // Incorrect answer
+    score.value -= question.value.value
+    alert('Incorrect. Try again!')
   }
+}
+
+  const goBack = () => {
+    router.push('/')
+    }
   </script>
   
   <style scoped>
