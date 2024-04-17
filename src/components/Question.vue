df1010d848a8e46c08053384de79dd4a91b35088
noamstul <stul0002@algonquinlive.com>
2024-04-17 10:33:45 -0400

    fixing routing

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -2,22 +2,38 @@
 <template>
     <div>
       <h2>Question</h2>
-      <p>{{ question.question }}</p>
-      <div v-for="(option, index) in question.options" :key="index" class="option" @click="checkAnswer(option)">
-        {{ option }}
+      <p v-if="question">{{ question.question }}</p>
+      <div v-if="question">
+        <div v-for="(option, index) in question.options" :key="index" class="option" @click="checkAnswer(option)">
+          {{ option }}
+        </div>
       </div>
+      <p v-else>No question found.</p>
     </div>
   </template>
   
   <script setup>
-  import { ref } from 'vue'
-  import { useRoute, useRouter } from 'vue-router'
+  import { ref, onMounted } from 'vue'
+  import { useRoute } from 'vue-router'
   import gameData from '../gameData.json'
   
-  const questionId = $route.params.id
-  const question = ref(gameData.questions.find(q => q.id === parseInt(questionId)))
+  const route = useRoute()
+  const questionId = ref(route.params.id)
+  const question = ref(null)
+  
+  onMounted(() => {
+    question.value = gameData.questions.find(q => q.id === parseInt(questionId.value))
+    if (!question.value) {
+      console.error(`Question with ID ${questionId.value} not found.`)
+    }
+  })
   
   const checkAnswer = (option) => {
+    if (!question.value) {
+      console.error('Question is not defined.')
+      return
+    }
+  
     if (option === question.value.answer) {
       // Correct answer
       // Add logic to update score or perform other actions
