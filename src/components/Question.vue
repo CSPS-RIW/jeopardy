60a9d19fd484b8ef600f8970c133788919757a27
noamstul <stul0002@algonquinlive.com>
2024-04-17 11:18:29 -0400

    adding disabled for questions once submitted

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -5,10 +5,10 @@
       <p v-if="question">{{ question.question }}</p>
       <form @submit.prevent="checkAnswer" v-if="question">
       <div v-for="(option, index) in question.options" :key="index" class="option">
-        <input type="radio" :id="'option_' + index" :value="option" v-model="selectedOption">
+        <input type="radio" :id="'option_' + index" :value="option" v-model="selectedOption" :disabled="isSubmitted">
         <label :for="'option_' + index">{{ option }}</label>
       </div>
-      <button type="submit" :disabled="!selectedOption">Submit</button>
+      <button type="submit" :disabled="!selectedOption || isSubmitted">Submit</button>
     </form>
       <p v-else>No question found.</p>
       <button @click="goBack" :disabled="!selectedOption">Back to Game Board</button>
@@ -30,6 +30,7 @@ const scoreStore = useScoreStore()
 
   const selectedOption = ref('')
 const score = ref(0)
+const isSubmitted = ref(false)
   
   onMounted(() => {
     question.value = gameData.questions.find(q => q.id === parseInt(questionId.value))
@@ -48,6 +49,7 @@ const score = ref(0)
     scoreStore.decreaseScore(question.value.value)
     alert('Incorrect. Try again!')
   }
+  isSubmitted.value = true
 }
 
   const goBack = () => {
