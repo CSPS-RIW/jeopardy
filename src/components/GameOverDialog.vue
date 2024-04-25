829cecc5b98a17fb952b79032643437774a2238b
noamstul <stul0002@algonquinlive.com>
2024-04-25 11:14:08 -0400

    adding styles to gameoverdialog

--- src/components/GameOverDialog.vue
+++ src/components/GameOverDialog.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="game-over-dialog">
-      <h2>Game Over</h2>
-      <p>Your final score is: {{ finalScore }}</p>
-      <button @click="$emit('update:retry')">Retry</button>
+      <div class="container">
+        <h2>Game Over</h2>
+        <p>Your final score is: {{ finalScore }}</p>
+        <button class="game-button" @click="$emit('update:retry')">Retry</button>
+      </div>
     </div>
   </template>
   
@@ -22,6 +24,22 @@
   /* Add your component-specific styles here */
   .game-over-dialog {
     text-align: center;
+    background-color: #000f52;
+    border-radius: 10px;
+    max-width: 700px;
+    min-height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+  }
+
+  .container {
+    padding: 2rem;
+    background-color: #060ce9;
+    color: #ffCC00;
+    border: 2px solid #ffCC00;
+    font-size: 20px;
   }
   </style>
   
