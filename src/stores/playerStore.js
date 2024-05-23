3acc989a43f6a2067c8d1e5e3da64f9169e60b7a
noamstul <stul0002@algonquinlive.com>
2024-05-23 13:30:48 -0400

    setting up the add player button for mp

--- src/stores/playerStore.js
+++ src/stores/playerStore.js
@@ -5,10 +5,29 @@ export const usePlayerStore = defineStore({
 	id: 'player',
 	state: () => ({
 		// Store user's progress
-		players: []
+		players: [],
+		playerCount: 0,
+		singlePlayerName: ''
 		
 	}),
 	actions: {
-		
+		addPlayer() {
+			this.playerCount++;
+
+			const newPlayer = {
+				name: '',
+				score: 0,
+				isPlayerTurn: false
+			}
+			if (this.playerCount <= 4) {
+				this.players.push(newPlayer)
+			} else {
+				alert('Maximum player count reached!')
+			}
+			
+		},
+		removePlayer() {
+
+		}
 	}
 });
--- src/views/Configuration.vue
+++ src/views/Configuration.vue
@@ -1,11 +1,53 @@
 <template>
   <div>
-    <!-- Your template code here -->
+    <h2>Game Configuration</h2>
+    <div>
+        <fieldset>
+            <div>
+                <input type="radio" name="option" id="single_player" value="single-player" v-model="gameMode">
+                <label for="single_player">Single Player</label>
+            </div>
+            <div>
+                <input type="radio" name="option" id="multi_player" value="multi-player" v-model="gameMode">
+                <label for="multi_player">Multi-Player</label>
+            </div>
+        </fieldset>
+    </div>
+    <div v-if="gameMode.length > 0">
+        <div >
+            <h3>Player Info</h3>
+            <div v-if="gameMode === 'multi-player'">
+                <form >
+                    <div v-for="(player, index) in playerStore.players" :key="index">
+                        <label :for="'player_name' + index">Player/Team Name: </label>
+                        <input type="text" name="player" :id="'player_name' + index" v-model="player.name">
+                    </div>
+                
+                </form>
+                <button @click="playerStore.addPlayer()">Add Player</button>
+            </div>
+            <div v-else-if="gameMode === 'single-player'">
+                <form>
+                    <span>
+                        <label for="player_name">Player Name: </label>
+                        <input type="text" name="player" id="player_name" v-model="playerStore.singlePlayerName">
+                    </span>
+                
+                </form>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-  // Your script setup code here
+  import { ref } from 'vue';
+    import { usePlayerStore } from '@/stores/playerStore';
+
+    const playerStore = usePlayerStore()
+
+    const gameMode = ref('')
+
 </script>
 
 <style scoped>
