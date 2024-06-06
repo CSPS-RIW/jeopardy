c80b4d39e61bb2f32671184889f4dc1677f15fd9
noamstul <stul0002@algonquinlive.com>
2024-06-06 09:18:52 -0400

    adding bootstrap, fa, styles

--- src/views/Configuration.vue
+++ src/views/Configuration.vue
@@ -1,43 +1,45 @@
 <template>
-    <div>
-      <h2>Game Configuration</h2>
-      <div>
-        <fieldset>
-          <div>
-            <input type="radio" name="option" id="single_player" value="single-player" v-model="gameMode">
-            <label for="single_player">Single Player</label>
-          </div>
-          <div>
-            <input type="radio" name="option" id="multi_player" value="multi-player" v-model="gameMode">
-            <label for="multi_player">Multi-Player</label>
-          </div>
-        </fieldset>
-      </div>
-      <div v-if="gameMode.length > 0">
+    <div class="flex">
+      <div class="config-container">
+        <h2 class="center">Game Configuration</h2>
         <div>
-          <h3>Player Info</h3>
-          <div v-if="gameMode === 'multi-player'">
-            <form>
-              <div v-for="(player, index) in playerStore.players" :key="index">
-                <label :for="'player_name' + index">Player/Team Name: </label>
-                <input type="text" name="player" :id="'player_name' + index" v-model="player.name">
-                <button @click.prevent="playerStore.deletePlayer(index)">Delete player</button>
-              </div>
-            </form>
-            <button @click="playerStore.addPlayer()" :disabled="playerStore.playerCount >= 4">Add Player</button>
-          </div>
-          <div v-else-if="gameMode === 'single-player'">
-            <form>
-              <span>
-                <label for="player_name">Player Name: </label>
-                <input type="text" name="player" id="player_name" v-model="playerStore.singlePlayerName">
-              </span>
-            </form>
+          <fieldset>
+            <div>
+              <input type="radio" name="option" id="single_player" value="single-player" v-model="gameMode">
+              <label for="single_player">Single Player</label>
+            </div>
+            <div>
+              <input type="radio" name="option" id="multi_player" value="multi-player" v-model="gameMode">
+              <label for="multi_player">Multi-Player</label>
+            </div>
+          </fieldset>
+        </div>
+        <div v-if="gameMode.length > 0">
+          <div>
+            <h3>Player Info</h3>
+            <div v-if="gameMode === 'multi-player'">
+              <form>
+                <div v-for="(player, index) in playerStore.players" :key="index">
+                  <label :for="'player_name' + index" class="mr-2">Player/Team Name: </label>
+                  <input class="mr-1" type="text" name="player" :id="'player_name' + index" v-model="player.name">
+                  <button class="delete-button" @click.prevent="playerStore.deletePlayer(index)"><i class="fas fa-times"></i></button>
+                </div>
+              </form>
+              <button @click="playerStore.addPlayer()" class="game-button" :disabled="playerStore.playerCount >= 4">Add Player</button>
+            </div>
+            <div v-else-if="gameMode === 'single-player'">
+              <form>
+                <span>
+                  <label for="player_name">Player Name: </label>
+                  <input class="ml-2" type="text" name="player" id="player_name" v-model="playerStore.singlePlayerName">
+                </span>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="startgame">
-        <router-link :disabled="gameMode === ''" to="/gameboard" @click="startGame">Start Game</router-link>
+        <div class="startgame" v-if="gameMode.length > 0">
+          <router-link to="/gameboard" @click="startGame">Start Game</router-link>
+        </div>
       </div>
     </div>
   </template>
@@ -45,6 +47,8 @@
   <script setup>
   import { ref } from 'vue';
   import { usePlayerStore } from '@/stores/playerStore';
+  import '@fortawesome/fontawesome-free/js/all.js';
+
   
   const playerStore = usePlayerStore();
   
@@ -57,6 +61,35 @@
   </script>
   
   <style scoped>
-  /* Your scoped styles here */
+    .disabled-link {
+      pointer-events: none;
+      color: #636363;
+    }
+
+    .center {
+      text-align: center;
+    }
+
+    .config-container {
+      padding: 2rem;
+      color: var(--game-button-blue);
+      border-radius: 10px;
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+      font-size: 20px;
+      max-width: 700px;
+    }
+
+    .flex {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .add-player {
+      padding: 0.2rem 0.7rem;
+      border: 0rem;
+      border-radius: 5px;
+    }
   </style>
   
