d67670e2675ceb011ce0b34bb3c24acc9a9abad1
noamstul <stul0002@algonquinlive.com>
2024-08-08 08:55:57 -0400

    #29 added animation to modal

--- src/App.vue
+++ src/App.vue
@@ -70,13 +70,5 @@ onBeforeMount(() => {
 </script>
 
 <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease-out;
-}
 
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
